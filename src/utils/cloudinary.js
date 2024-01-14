23dec099bc5bbdae7fda842147f8aa641e612ea1
Pritam <pri10145@gmail.com>
2024-01-14 18:50:19 +0530

    file upload with cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_KEY_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // upload the file on the cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been successfully uploaded
+    console.log(`File is uploaded successfully on cloudinary, ${response.url}`);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally save temporary file as the upload operation got rejected
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
