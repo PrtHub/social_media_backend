1d5bb97a8a14611e50ccfa1f712a9a79efae0e7c
Pritam <pri10145@gmail.com>
2024-01-15 18:59:47 +0530

    router and controller

--- src/app.js
+++ src/app.js
@@ -1,18 +1,27 @@
-import express from 'express';
-import cors from 'cors'
-import cookieParser from 'cookie-parser';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-}))
+  })
+);
 
-app.use(express.json({ limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static('public'))
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
-export { app}
+//import routes
+
+import userRouter from "./routes/user.routes.js";
+
+// declarations
+app.use("/api/v1/users", userRouter);
+
+export { app };
