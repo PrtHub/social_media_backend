88111240cbea8cee2c8a04175a63880f4265a170
Pritam <pri10145@gmail.com>
2024-01-26 18:25:57 +0530

    add healthCheck

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ import tweetRouter from './routes/tweet.routes.js'
 import likeRouter from './routes/like.routes.js'
 import playlistRouter from './routes/playlist.routes.js'
 import dashboardRouter from './routes/dashboard.routes.js'
+import healthcheckRouter from './routes/healthcheck.routes.js'
 
 // declarations
 app.use("/api/v1/users", userRouter);
@@ -37,5 +38,6 @@ app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,14 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const healthcheck = asyncHandler(async (req, res) => {
+  try {
+    res.status(200).json(new ApiResponse(200, "OK", "Server is up and running"));
+  } catch (error) {
+    console.error(error);
+    throw new ApiError(500, "Internal server error");
+  }
+});
+
+export { healthcheck };
